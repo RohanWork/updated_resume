89e4b8896625f585c7a4c17087261317a74539b2
Rohan Katare <88019506+RohanWork@users.noreply.github.com>
2024-04-15 21:29:32 +0530

    Add files via upload

---
+++ resume/comp/script.js
@@ -0,0 +1,16 @@
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+        // // Add event listener for keydown event
+        // document.addEventListener('keydown', function(event) {
+        //     // Check if the pressed key is the Print Screen key (code 44) or the Take Screenshot key (code 45)
+        //     if (event.keyCode === 44 || event.keyCode === 45) {
+        //         // Display a message informing the user that screenshots are not allowed
+        //         alert('Screenshots are not allowed on this webpage.');
+        //         // Prevent the default behavior of the key press
+        //         event.preventDefault();
+        //         // Optionally, you can also prevent the context menu from opening when right-clicking
+        //         document.addEventListener('contextmenu', function(event) {
+        //             event.preventDefault();
+        //         });
+        //     }
+        // });
